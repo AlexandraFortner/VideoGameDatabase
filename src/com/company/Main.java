a3ab24ab909adde3a1ff56a1d628fd3712d1064c
Alexandra <afortner@basecampcodingacademy.org>
2018-03-07 11:33:31 -0600

    Make beginning input to show all game titles.
    Need to display all game publishers. Maybe use count to prevent multiple lines of the same publisher. I plan to do the same to publication years.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,5 @@
 package com.company;
-
+import java.util.*;
 import java.sql.*;
 
 public class Main {
@@ -7,16 +7,23 @@ public class Main {
         try {
             Connection c = DriverManager.getConnection(
                     "jdbc:postgresql:VideoGameDatabase", "basecamp", "pgpass");
-            PreparedStatement st = c.prepareStatement("SELECT * FROM games;");
-            ResultSet rs = st.executeQuery();
-            while (rs.next()) {
-                System.out.println(rs.getString("title"));
+//            Beginning Input: Program Asks which database to access
+            Scanner reader = new Scanner(System.in);  // Reading from System.in
+            System.out.println("Welcome to The Video Game Database!\n\nIf you would like " +
+                    "to see all available game titles,\ninput the number 1.\n\n");
+            int input1 = reader.nextInt();
+            if (input1 == 1){
+                PreparedStatement st = c.prepareStatement("SELECT * FROM games;");
+                ResultSet rs = st.executeQuery();
+                while (rs.next()) {
+                    System.out.println(rs.getString("title"));
+                }
             }
+
         } catch (Exception e) {
             e.printStackTrace();
             System.err.println(e.getClass().getName()+": "+e.getMessage());
             System.exit(0);
         }
-        System.out.println("Opened database successfully");
     }
 }
