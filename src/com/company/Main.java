d441d228a6159ffa26483eea3f1e60a108437b58
Alexandra <afortner@basecampcodingacademy.org>
2018-03-07 12:05:44 -0600

    Displayed all game publishers using count to show how many games come from each publisher.
    I plan to do the same for publication year.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,13 +10,29 @@ public class Main {
 //            Beginning Input: Program Asks which database to access
             Scanner reader = new Scanner(System.in);  // Reading from System.in
             System.out.println("Welcome to The Video Game Database!\n\nIf you would like " +
-                    "to see all available game titles,\ninput the number 1.\n\n");
-            int input1 = reader.nextInt();
-            if (input1 == 1){
-                PreparedStatement st = c.prepareStatement("SELECT * FROM games;");
-                ResultSet rs = st.executeQuery();
-                while (rs.next()) {
-                    System.out.println(rs.getString("title"));
+                    "to see the Games library, enter: 1.\nIf you would like to " +
+                    "see all available users, enter: 2.\nIf you would like to " +
+                    "see what users have certain games, enter: 3.\n\n");
+            int libraryChoosing = reader.nextInt();
+            if (libraryChoosing == 1){
+                System.out.println("\nIf you would like " +
+                        "to see all available game titles, enter: 1.\nIf you would like " +
+                        "to see all game publishers, enter: 2.\n\n");
+                int gameLibrary = reader.nextInt();
+                if (gameLibrary == 1){
+                    PreparedStatement st = c.prepareStatement("SELECT * FROM games;");
+                    ResultSet rs = st.executeQuery();
+                    while (rs.next()) {
+                        System.out.println(rs.getString("title"));
+                    }
+                } else if (gameLibrary == 2){
+                    PreparedStatement st = c.prepareStatement("SELECT publisher, count(*) as NUM from" +
+                            " games group by publisher;");
+                    ResultSet rs = st.executeQuery();
+                    while (rs.next()) {
+                        System.out.print(rs.getString("publisher"));
+                        System.out.println(rs.getInt("NUM"));
+                    }
                 }
             }
 
